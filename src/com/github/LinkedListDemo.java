60ed73f624b2ff0ba4cc46ba445be7f55dded061
Vicky Sonewane <vickysonwane800@gmail.com>
2022-12-25 06:55:41 +0530

    second commit: LinkedList Demo

--- src/com/github/LinkedListDemo.java
+++ src/com/github/LinkedListDemo.java
@@ -1,5 +1,7 @@
 package com.github;
 
+import java.awt.List;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedList;
 
@@ -11,11 +13,10 @@ public class LinkedListDemo {
 		list.add(20);
 		list.add(30);
 		
-	Iterator<Integer> itr1=list.iterator();
-	while(itr1.hasNext()) {
-		System.out.println(itr1.next());
-		
-	}
+		Iterator<Integer> itr=list.iterator();
+		while(itr.hasNext()) {
+			System.out.println(itr.next());
+		}
 
 	}
 
